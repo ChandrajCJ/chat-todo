5c3581af7fb8a489aacca0163dfb71bebcfafb7f
Chandraj1710 <chandraj.neelakandan@contentstack.com>
2025-02-01 21:26:09 +0530

    Initial commit or update

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { TodoList } from './components/TodoList';
 function App() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
-      <header className="bg-white border-b border-gray-200 p-4 sticky top-0">
+      <header className="bg-white border-b border-gray-200 z-50 p-4 sticky top-0">
         <h1 className="text-xl font-bold text-center">Community Medicine</h1>
       </header>
       <main className="flex-1 flex flex-col max-w-2xl mx-auto w-full">
